b2bc98de7255c7d1adb559f2bf8e74f3be90b639
Daniel Hueholt <dmhuehol@ncsu.edu>
2025-02-18 12:59:52 -0700

    Update GDD calculation to use classes for CESM
    GDD calculation now uses classes for CESM output. Additionally, edit documentation for clarity and remove unused code and imports.

--- calc_gdd.py
+++ calc_gdd.py
@@ -2,39 +2,37 @@
 Calculate growing degree days from daily temperature data for an input 
 base value and save as netCDF.
 
-Configured to work with the following data types:
+Configured for the following data types:
 1) Output from the Community Earth System Model (e.g., LENS2)
 2) Station data from the Global Historical Climatology Network (GHCN)
 3) Station data from the Danish Meteorological Institute (DMI)
 4) Reanalysis from the European Reanalysis 5 (ERA5)
 
-In the work which this codebase accompanies, only 1) and 2) are used.
-Additional datasets were part of exploratory analysis and may prove 
-useful again in the future.
-
-Sum, number, max, datetimes
+Only 1) and 2) are directly used in the paper this codebase accompanies.
+3) and 4) were part of exploratory analysis and may prove useful again 
+in the future.
 '''
-########################################################################
 import glob
 import sys
 
 from icecream import ic
-import numpy as np
 import xarray as xr
 
 import classes_gddt as cg
 import fun_calc_var as fcv
 
-d_d_gen = {
-    "p": '/Volumes/Polycrystal/Data/LENS2/daily_TREFHT/refined/',
-    "tok": '*.nc',
-    "var": 'TREFHT'
-}
-d_d_hpc_lens2 = {
-    "p": '/barnes-engr-scratch1/DATA/CESM2-LE/temp/refined_nomask/',
-    "tok": '*.nc',
-    "var": 'TREFHT'
-}
+dp_cesm = cg.DataParams(
+    path='/Volumes/Polycrystal/Data/LENS2/daily_TREFHT/refined/',
+    tok='*arc*.nc', var='TREFHT', flag_raw_ds=True, flag_raw_da=True, 
+    flag_time_slice=True, flag_manage_rlz=False, flag_land_mask=False,
+    flag_roi=False
+)
+dp_cesm_hpc = cg.DataParams(
+    path='/barnes-engr-scratch1/DATA/CESM2-LE/temp/refined_nomask_orig/',
+    tok='*arc*.nc', var='TREFHT', flag_raw_ds=True, flag_raw_da=True, 
+    flag_time_slice=True, flag_manage_rlz=False, flag_land_mask=False,
+    flag_roi=False
+)
 dp_dmi = cg.DataParams(
     path='/Users/dhueholt/Documents/gddt_data/Greenland_Obs/',
     tok='*.nc', var='mean_temp', flag_raw_ds=True, flag_raw_da=True, 
@@ -48,26 +46,11 @@ dp_era5 = cg.DataParams(
     flag_roi=False)
 #  ndd_flag False for model output or reanalysis, True for station obs
 gddp = cg.GddParams(
-    base=5, ndd_flag=True, out_flag=True, out_filename=None,
-    out_path='/Users/dhueholt/Documents/gddt_data/gdd/dmi/',
-    # out_path='/barnes-engr-scratch1/DATA/CESM2-LE/processed_data/annual/gdd/reproc_20241015/',
+    base=5, ndd_flag=False, out_flag=True, out_filename=None,
+    # out_path='/Users/dhueholt/Documents/gddt_data/gdd/lens2/',
+    out_path='/barnes-engr-scratch1/DATA/CESM2-LE/processed_data/annual/gdd/reproc_20250218/',
     out_var_name='auto')
-
-# out_d = {
-#     "flag": True,
-#     "p": '/Users/dhueholt/Documents/gddt_data/gdd/era5/',
-#     "o_name": None,
-#     "var": 'gdd' + str(gdd_d["base"]) + '_sum'
-# }
-# out_d_hpc = {
-#     "flag": True,
-#     # "p": '/barnes-engr-scratch1/DATA/CMIP6/processed/CMIP/piControl/day_1/gdd_base5/',
-#     "p": '/barnes-engr-scratch1/DATA/CESM2-LE/processed_data/annual/gdd/reproc_20241015/',
-#     "o_name": None,
-#     "var": 'gdd' + str(gdd_d["base"]) + '_sum'
-# }
-dp = dp_dmi
-# out_d = out_d_hpc
+dp = dp_cesm_hpc
 
 globs = sorted(glob.glob(dp.path + dp.tok))
 for f in globs:
@@ -77,7 +60,7 @@ for f in globs:
     da_gdd_sum = fcv.gdd(da_in, gddp)
     da_ndd_sum = fcv.ndd(da_in)
     try:
-        #  Adds station data for GHCN data
+        #  Adds station metadata for GHCN observations
         station = da_in.attrs['station']
     except (AttributeError, KeyError):
         station = 'n/a'
